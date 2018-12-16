b6a9951612bb034c44b60fa745760274ecab739e
Cotes <cotes.brandon@yahoo.com>
2018-12-16 18:50:19 -0500

    pseduocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// varibles for comp to choose from
+// varibles assigned to images
+// wins/losses set to 0
+// reset
+// comp chooses random number 
+// random values assigned to images
+// make images clickable
+// when image clicked number assigned to the image that is clicked will show
+// adds together values of the image clicked for score
+
