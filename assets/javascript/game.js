d399f2299fd7fd95b67c916b8edf2db86fe01346
Cotes <cotes.brandon@yahoo.com>
2018-12-18 15:22:05 -0500

    finsihed game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,120 @@
 // varibles for comp to choose from
-var $guessedNumber = 53;
+var $guessedNumber = Math.floor(Math.random() * 100) +52;
 
 $(".guessed-number").text($guessedNumber);
 
+// counter for game 
 var counter = 0;
+var wins = 0;
+var losses = 0;
 
-// varibles assigned to images
-var numberOptions = Math.floor(Math.random() * 27) +1;
+// reset function
+function reset() {
+  $guessedNumber = Math.floor(Math.random() * 100) +52;
+  $('.guessed-number').text($guessedNumber);
+  counter = 0;
+  numberOptions = [];
+  numberOptions2 = [];
+  numberOptions3 = [];
+  numberOptions4 = [];
 
-// loop to create image for every option
-for (var i = 0; i < numberOptions; i++) {
-  var dragonBall = $("<img>");
+  $(".dragon-ball").each(function() {
+    var randomNumber = Math.floor(Math.random() * 10);
 
-  dragonBall.addClass("dragon-ball");
+    $(this).attr("data-ballvalue", randomNumber);
+  })
 
-  dragonBall.attr("src", "assets/images/dragonball.jpeg");
-
-  dragonBall.attr("data-ballvalue", numberOptions);
-
-  $(".images").append(dragonBall);
+  $(".score").empty();
 }
 
+// reset button
+$("#reset-button").on("click", function() {
+  reset();
+});
+
+
+// varibles assigned to first image
+var numberOptions = Math.floor(Math.random() * 13) +1;
+
+// creating first image
+  var dragonBall1 = $("<img>");
+
+  // add class for css attributes to image
+  dragonBall1.addClass("dragon-ball");
+
+  // linking image
+  dragonBall1.attr("src", "assets/images/dragonball.jpeg");
+
+  // creating data attributes for images
+  dragonBall1.attr("data-ballvalue", numberOptions);
+
+  // printing image to screen
+  $(".images").append(dragonBall1);
+
+// image 2
+var numberOptions2 = Math.floor(Math.random() * 13) +1;
+
+
+  var dragonBall2 = $("<img>");
+
+  dragonBall2.addClass("dragon-ball");
+
+  dragonBall2.attr("src", "assets/images/dragonball.jpeg");
+
+  dragonBall2.attr("data-ballvalue", numberOptions2);
+
+  $(".images").append(dragonBall2);
+
+// image 3
+var numberOptions3 = Math.floor(Math.random() * 13) +1;
+
+
+  var dragonBall3 = $("<img>");
+
+  dragonBall3.addClass("dragon-ball");
+
+  dragonBall3.attr("src", "assets/images/dragonball.jpeg");
+
+  dragonBall3.attr("data-ballvalue", numberOptions3);
+
+  $(".images").append(dragonBall3);
+
+// image 4
+var numberOptions4 = Math.floor(Math.random() * 13) +1;
+
+  var dragonBall4 = $("<img>");
+
+  dragonBall4.addClass("dragon-ball");
+
+  dragonBall4.attr("src", "assets/images/dragonball.jpeg");
+
+  dragonBall4.attr("data-ballvalue", numberOptions4);
+
+  $(".images").append(dragonBall4);
+
+
+// runs when an image is clicked 
 $(".dragon-ball").on("click", function(){
 
+   
   var ballValue = ($(this).attr("data-ballValue"));
   ballValue = parseInt(ballValue);
 
   counter += ballValue;
 
-  alert("New score: " + counter);
+  $(".score").text("New score: " + counter);
 
   if (counter === $guessedNumber) {
-    alert("You win!");
+    $(".score").text("You win!");
+    wins++;
+    
+    
   }
 
   else if (counter >= $guessedNumber) {
-    alert("you lose!");
+    $(".score").text("you lose!");
+    losses++;
+    $(".player-score").text("Losses: " + losses);
   }
 
 });
